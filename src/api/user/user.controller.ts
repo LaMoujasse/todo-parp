9c4cb62ec9cae0579751c4cf5b61088791dc62a2
ToDoList <todolist@pop-os.localdomain>
2023-07-13 12:21:42 +0200

    Add @Delete on user table

--- src/api/user/user.controller.ts
+++ src/api/user/user.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Get, Param, Post } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Post } from '@nestjs/common';
 import { UserService } from './user.service';
 import { User } from 'src/entities/user.entity';
 
@@ -16,6 +16,13 @@ export class UserController {
     @Param('id') id
   ): Promise<User> {
     return this.userService.findOne(id);
+  }
+
+  @Delete(':id')
+  async remove(
+    @Param('id') id
+  ){
+    return await this.userService.remove(id);
   }
 
   @Post()
